4c74ef6bb7f17837a0f0333cfd106c5918412f70
hhs4648 <hhs4648@gmail.com>
2026-09-02 22:59:29 +0900

    Add Capacitor iOS configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.haksup.haksup_app',
+  appName: '학습',
+  webDir: 'dist',
+}
+
+export default config
